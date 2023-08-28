33e10a205fbe30c92f286d301d0d9af3d47d444f
Tykhonenko Yana <tihonenkoyana@gmail.com>
2023-08-28 18:51:46 +0200

    add new endpoint GetBooks

--- src/main/java/de/ait/gr5/bs/services/BooksService.java
+++ src/main/java/de/ait/gr5/bs/services/BooksService.java
@@ -1,12 +1,13 @@
 package de.ait.gr5.bs.services;
 
-import de.ait.gr5.bs.dto.BookDto;
-import de.ait.gr5.bs.dto.BookNewDto;
-import de.ait.gr5.bs.dto.BookUpdateDto;
+import de.ait.gr5.bs.dto.*;
+
+import java.util.List;
 
 public interface BooksService {
   BookDto addBook(BookNewDto newBook);
 
   BookDto updateBook(Long bookId, BookUpdateDto updateBook);
 
+  BooksShortDto getBooks(Long userId);
 }
