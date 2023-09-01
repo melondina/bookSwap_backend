3ca3763f8216e5f3e79c45b6df11fe783e083e0d
Tykhonenko Yana <tihonenkoyana@gmail.com>
2023-09-01 15:44:34 +0200

    refactoring

--- src/main/java/de/ait/gr5/bs/dto/BookDto.java
+++ src/main/java/de/ait/gr5/bs/dto/BookDto.java
@@ -7,10 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
