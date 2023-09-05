9aa58c65bc40e6272edb2e3584300b1c57caf2dd
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-09-05 15:32:27 +0200

    update UsersControllerIntegrationTestControllerIntegrationTests

--- src/test/java/de/ait/gr5/bs/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/de/ait/gr5/bs/controllers/UsersControllerIntegrationTest.java
@@ -41,7 +41,6 @@ public class UsersControllerIntegrationTest {
     void update_current_user() throws Exception {
 
         String body = objectMapper.writeValueAsString(UpdateUserDto.builder()
-                .newRole("USER")
                 .postalCode("78224")
                 .build());
 
@@ -62,7 +61,6 @@ public class UsersControllerIntegrationTest {
     void update_another_user() throws Exception {
 
         String body = objectMapper.writeValueAsString(UpdateUserDto.builder()
-                .newRole("USER")
                 .firstName("Andy")
                 .postalCode("78224")
                 .build());
@@ -82,7 +80,6 @@ public class UsersControllerIntegrationTest {
     @Test
     @WithUserDetails(value = "test@mail.com")
     @Sql(scripts = "/sql/data_for_users.sql")
-  //  @WithMockUser(username = "first.user@gmail.com", password = "Qwerty007!")
     @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     public void getProfileReturnsOkForAuthenticatedUser() throws Exception {
 
